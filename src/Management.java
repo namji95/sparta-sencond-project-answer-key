8cf2e895bae5c2d827f1049181340e55194aeb45
gloz0315 <qudtn0315@naver.com>
2024-01-15 11:40:47 +0900

    수강생을 등록, 수정, 조회하는 클래스 생성 및 서로의 코드를 합치기 위한 기틀 과정 생성

--- src/Management.java
+++ src/Management.java
@@ -1,16 +1,31 @@
 import console.Console;
+import service.Information;
+import service.Modify;
+import service.Register;
 import view.Input;
 
 public class Management {
+
+    private Register register;
+    private Modify modify;
+    private Information information;
+
+    public Management() {
+        register = new Register();
+        modify = new Modify();
+        information = new Information();
+    }
 
     // 실행할 때 해당 메서드를 중심으로 실행
     public void run() {
         while(true) {
             System.out.println("메인 화면입니다."); // 여기는 나중에 수정할 예정
-            System.out.println("1. 등록하기    2. 수정하기    3. 조회하기    4. 나가기");
+            System.out.println("1. 등록하기    2. 수정하기    3. 조회하기    0. 나가기");
             int number = Integer.parseInt(Input.inputNumber());
 
-            if(number==4)
+            switchCase(number);
+
+            if(number==0)
                 break;
         }
 
@@ -22,13 +37,19 @@ public class Management {
 
     private void switchCase(int number) {
         switch (number) {
-            case 1:                 // 회원 등록하기
+            case 1:
+                System.out.println("수강생을 등록하겠습니까?");
+                register.registerStudent(); // 수강생 등록하기
                 break;
 
-            case 2:                 // 회원 수정하기
+            case 2:
+                System.out.println("수강생을 수정하시겠습니까?");
+                modify.modify();            // 수강생 수정하기
                 break;
 
-            case 3:                 // 회원 조회하기
+            case 3:
+                System.out.println("수강생을 조회하시겠습니까?");
+                information.lookUpInformation(); // 수강생 조회하기
                 break;
 
             default:
