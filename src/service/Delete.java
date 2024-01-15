37652e3b48f9ca46d2481970e1da660d37847cca
gloz0315 <qudtn0315@naver.com>
2024-01-15 17:45:22 +0900

    refactor(Delete) : 삭제에 대한 클래스 기능 리팩토링

--- src/service/Delete.java
+++ src/service/Delete.java
@@ -3,30 +3,23 @@ package service;
 import console.Console;
 import domain.Student;
 import domain.StudentData;
-
-import java.util.List;
+import invalidate.Invalidate;
 
 public class Delete {
 
-    public void deleteStudent(List<Student> students) {
-        System.out.println("\n 삭제할 수강생 번호를 입력하세요");
-        long accountId = Console.inputInt();
-        Student foundStudent = null;
+    public void deleteStudent(StudentData studentData) {
+        while(true) {
+            System.out.print("\n삭제할 수강생 번호를 입력하세요: ");
+            String number = Console.inputString();
+            System.out.println("수강생이 있는지 조회하고 있습니다...");
 
-        for(Student student : students) {
-            if(student.getAccountId() == accountId ) {
-                foundStudent = student;
-                break;
-            }
+            if(!Invalidate.invalidateNumber(number) || Invalidate.invalidateStudent(number))
+                continue;
+
+            Student removeStudent = studentData.getStudentInfo(Long.parseLong(number));
+            studentData.getStudents().remove(removeStudent);
+            System.out.println("학생 정보가 삭제되었습니다!\n");
+            break;
         }
-
-        if (foundStudent != null) {
-            students.remove(foundStudent);
-            System.out.println("학생 정보가 삭제되었습니다.");
-        } else {
-            System.out.println("학생 정보가 없습니다. 다시 입력해주세요.");
-        }
-
-
     }
 }
